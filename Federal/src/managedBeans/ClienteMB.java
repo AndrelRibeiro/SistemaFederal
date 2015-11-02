2a4b4be59d78aebff8442702e0135724473d49e1
AndrelRibeiro <andre.ribeiro6@fatec.sp.gov.br>
2015-11-01 23:52:01 -0200

    Atualização

--- Federal/src/managedBeans/ClienteMB.java
+++ Federal/src/managedBeans/ClienteMB.java
@@ -8,7 +8,6 @@ import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.faces.context.FacesContext;
-
 import beans.Beneficiario;
 import beans.Cliente;
 import beans.Contrato;
